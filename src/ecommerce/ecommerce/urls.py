cacc18f062565c9b8b0c566f7c3e7af8b422e3d7
pankaj0812 <singhpankajkumar65@gmail.com>
2018-01-28 18:13:12 +0530

    added slugfields and random string generator for url

--- src/ecommerce/ecommerce/urls.py
+++ src/ecommerce/ecommerce/urls.py
@@ -18,7 +18,15 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.conf.urls import url
 from django.contrib import admin
-from products.views import ProductListView , product_list_view, ProductDetailView, product_detail_view
+from products.views import (
+    ProductListView ,
+    product_list_view,
+    ProductDetailView,
+    product_detail_view,
+    ProductFeaturedDetailView,
+    ProductDetailSlugView,
+    ProductFeaturedListView
+)
 from .views import home_page,about_page,contact_page,login_page,register_page
 
 
@@ -28,9 +36,12 @@ urlpatterns = [
     url(r'^contact/$', contact_page),
     url(r'^login/$', login_page),
     url(r'^register/$', register_page),
+    url(r'^featured/$', ProductFeaturedListView.as_view()),
+    url(r'^featured/(?P<pk>\d+)/$',ProductFeaturedDetailView.as_view()),
     url(r'^products/$', ProductListView.as_view()),
     url(r'^products-fbv/$', product_list_view),
-    url(r'^products/(?P<pk>\d+)/$', ProductDetailView.as_view()),
+    # url(r'^products/(?P<pk>\d+)/$', ProductDetailView.as_view()),
+    url(r'^products/(?P<slug>[\w-]+)/$', ProductDetailSlugView.as_view()),
     url(r'^products-fbv/(?P<pk>\d+)/$', product_detail_view),
     url(r'^admin/', admin.site.urls),
 ]
--- src/ecommerce/products/views.py
+++ src/ecommerce/products/views.py
@@ -5,6 +5,23 @@ from .models import Product
 
 
 # Create your views here.
+class ProductFeaturedListView(ListView):
+    template_name = "products/list.html"
+
+    def get_queryset(self, *args, **kwargs):
+        request = self.request
+        return Product.objects.all().featured()
+
+
+class ProductFeaturedDetailView(DetailView):
+    queryset = Product.objects.all().featured()
+    template_name = "products/featured-detail.html"
+
+    # def get_queryset(self, *args, **kwargs):
+    #     request = self.request
+    #     return Product.objects.featured()
+
+
 class ProductListView(ListView):
     # queryset = Product.objects.all()
     template_name = "products/list.html"
@@ -24,6 +41,26 @@ def product_list_view(request):
         'object_list' : queryset
     }
     return render(request, "products/list.html", context)
+
+
+class ProductDetailSlugView(DetailView):
+    queryset = Product.objects.all()
+    template_name = "products/detail.html"
+
+    def get_object(self, *args, **kwargs):
+        request = self.request
+        slug = self.kwargs.get('slug')
+        # instance = get_object_or_404(Product, slug=slug, active=True)
+        try:
+            instance = Product.objects.get(slug=slug, active=True)
+        except Product.DoesNotExist():
+            raise Http404("Not Found.. ")
+        except Product.MultipleObjectsReturned:
+            qs = Product.objects.filter(slug=slug, active=True)
+            instance = qs.first()
+        except:
+            raise Http404("Uhmmm")
+        return instance
 
 
 class ProductDetailView(DetailView):
@@ -50,8 +87,8 @@ class ProductDetailView(DetailView):
 
 
 def product_detail_view(request, pk=None, *args, **kwargs):
-    # instance = Product.objects.get(pk=pk)
-    # instance = get_object_or_404(Product, pk=pk)
+    # instance = Product.objects.get(pk=pk, featured=True)
+    # instance = get_object_or_404(Product, pk=pk, fetaured=True)
     # try:
     #     instance = Product.objects.get(id=pk)
     # except Product.DoesNotExist:
